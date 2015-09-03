8f3a5a08e2aef0eb20fe03b0aa7f65435a9f41ac
J Siren <quarkchasermusic@gmail.com>
2015-09-03 11:17:23 -0700

    basic person code

--- js/person.js
+++ js/person.js
@@ -1,1 +1,27 @@
 // person.js
+
+// person object needs:
+
+// name 
+// userName
+// badges
+// gravatar address - linked to treehouse - has profile photo
+
+
+var Person = (function (){
+	function Person(){
+		this.name = '';
+		this.username = '';
+		this.badges = [];
+		this.gravatar = '';
+		
+	}
+
+	Person.prototype.numBadges = function(){
+		return this.badges.length;
+	}
+
+	return Person;
+
+})();
+
