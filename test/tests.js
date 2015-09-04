3ef9e4e106945165606ef01d257aaef26482cd96
J Siren <quarkchasermusic@gmail.com>
2015-09-04 13:57:40 -0700

    added some new tests

--- test/tests.js
+++ test/tests.js
@@ -11,53 +11,83 @@ if (typeof require !== 'undefined'){
 var expect = chai.expect;
 
 describe('Testing the tester', function() {
-    var num = 9
-    it('should ', function(){
+    var num = 9;
+    it('should pass this simple test', function(){
       expect(num).to.equal(9);
     })
+
 });
 
-// test making a person object, should be an object
-// see that is an instance of person
-
-
-
-// describe('Testing the Person Ctor', function() {
+describe('Testing the Person Ctor', function() {
     
-//     var jason = new Person();
+    var jason = new Person();
 
-//     jason.name = 'jason'
-//     jason.username = 'jasonsiren'
+    jason.name = 'jason'
+    jason.username = 'jasonsiren'
 
-//     it('should be an Object', function(){
-//         expect(jason).to.be.an('object');
+    it('should be an Object', function(){
+        expect(jason).to.be.an('object');
       
 
-//     })
-//     it('should have a class of Person', function(){
-//         expect(jason.constructor).to.equal(Person);
+    })
+    it('should have a class of Person', function(){
+        expect(jason.constructor).to.equal(Person);
 
-//     })
+    })
 
-//     it ('should have a name value of jason', function(){
-//         expect(jason)
-//     })
+    it ('should have a name value of jason', function(){
+        expect(jason)
+    })
 
-// // make an ajax call for a singular person,
-// // see that it returns a json file
-// // google aynchronous tests in mocha chai
+// make an ajax call for a singular person,
+// see that it returns a json file
+// google aynchronous tests in mocha chai
+});
+
+describe('Testing the controller Ctor', function() {
+
+    var controller;
+    
+    before(function(done){
+        controller = new Controller();
+        controller.getAndProcessData()
+        controller.completedCallback = function(){
+            done();
+        }
+    })
+    
+
+    it('should contain an array of people', function(){
+        expect(controller.people).to.be.instanceof(Array);
+
+    })
+    it('The people array length should be 14', function(){
+        expect(controller.people.length).to.equal(14);
+
+    })
+
+    it('the people array should contain a person at index 0', function(){
+        expect(controller.people[0]).to.be.instanceof(Person);
+    })
+
+});
 
 
-// });
 
-// describe('Testing the controller Ctor', function() {
 
-//     var controller = new Controller();
-//     controller.getAndProcessData();
 
-//     it('should contain an array of people', function(){
-//         expect(controller.people).to.be.instanceof(Array);;
+// Controller.prototype.getAndProcessData = function()
 
-//     })
-// });
+// Controller.prototype.getPersonByUsername = function (name)
 
+// Controller.prototype.badgesOfPerson = function(person)
+
+// Controller.prototype.intersectBadges = function (p1, p2)
+
+// Controller.prototype.getUnearnedBadgesFor = function (person)
+
+// Controller.prototype.similarity = function (p1, p2)
+
+// Controller.prototype.score = function (badge, person)
+
+// Controller.prototype.getRecommendationsFor = function (person)
