756327c575ec565e93fa2caf534e2350215cc5ac
J Siren <quarkchasermusic@gmail.com>
2015-09-04 10:29:18 -0700

    first broken test

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,17 @@
 // TESTS!
+
+if (typeof require !== 'undefined'){var Person = require('../person.js');
+var Badge = require('../badge.js');
+var Controller = require('../controller.js');
+
+var chai = require('chai');
+var expect = chai.expect;
+
+};
+
+describe('Testing the tester', function() {
+    it('should ', function(){
+      expect(9).to.equal(9);
+    })
+});
+
